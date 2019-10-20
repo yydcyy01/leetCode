c6b2c658066796c71281eeefa5153a6ddaa2efac
yuyang <447395536@qq.com>
2019-10-20 23:27:10 +0800

    160

--- src/com/yydcyy/list/_160_Intersection_of_Two_Linked_Lists.java
+++ src/com/yydcyy/list/_160_Intersection_of_Two_Linked_Lists.java
@@ -3,8 +3,14 @@ package com.yydcyy.list;
 import com.yydcyy.list.utils.ListNode;
 
 /**
+ * https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+ *
  * @author YYDCYY
  * @create 2019-10-16
+ *     time O(N)
+ *     space O(1)
+ *     设 A 长度 a+c , B 长度 b+c,其中 c 为尾部公共长度, 可知 a+b+c = b+c+a
+ *     若只是判断是否存在交点(此提示找到这个交点),则是另一题:编程之美 3.6.
  */
 public class _160_Intersection_of_Two_Linked_Lists {
     //public class Solution {
@@ -19,3 +25,4 @@ public class _160_Intersection_of_Two_Linked_Lists {
         }
     //}
 }
+
