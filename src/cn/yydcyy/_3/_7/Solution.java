f17d9faf7680ab6e3763e4fb9d11fd1adee94aad
yuyang <447395536@qq.com>
2019-12-18 18:28:29 +0800

    LC 392

--- src/cn/yydcyy/_3/_7/Solution.java
+++ src/cn/yydcyy/_3/_7/Solution.java
@@ -1,7 +1,10 @@
 package cn.yydcyy._3._7;
 
 /**
- *
+ *LC 392 : 解题思路 : \
+ *  在 String.t 中 挨个索引String.s [由题意, 可以不连续, 比如 "abc" "ahbgdc"查a 在 t 中索引 : index=0, 下次从 0+1 开始查 b 索引, 返回 2 下次从 2+1 开始查 c 索引, 一直查到最后 过程中只有查到最后(返回-1)直接 return false,
+ *  否则就是子串, return true.    用好 String 工具类. index() , charAt 等等方法. 效率贼高.
+ *  ]
  *
  *
  *
