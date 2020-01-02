8fde5cc81318d5843da810e5851142966ad6f885
yuyang <447395536@qq.com>
2020-01-02 10:37:37 +0800

    LC 462

---
+++ src/cn/yydcyy/_1/_8/_1/_7/_1/Solution.java
@@ -0,0 +1,23 @@
+package cn.yydcyy._1._8._1._7._1;
+
+import java.util.Arrays;
+
+/**
+ * LC 462
+ 相遇问题, 每次最多 ++ -- , 问移动多少次可以把所以元素变成全相同.
+ nums[R] - nums[L] = (nums[R] - N) + (N - nums[L] );
+*/
+class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int L = 0; //左边界
+        int R = nums.length - 1; // 右边界
+        int res = 0;
+        while (L <= R) {
+            res += nums[R] - nums[L];
+            L ++;
+            R --;
+        } 
+        return res;
+    }
+}
