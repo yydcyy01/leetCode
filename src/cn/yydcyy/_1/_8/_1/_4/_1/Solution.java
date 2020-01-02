1edd319ffdb2c7d642f2adcd24c0861280cfea6e
yuyang <447395536@qq.com>
2020-01-02 09:07:20 +0800

    LC 168

---
+++ src/cn/yydcyy/_1/_8/_1/_4/_1/Solution.java
@@ -0,0 +1,14 @@
+package cn.yydcyy._1._8._1._4._1;
+
+/**
+ * LeetCode 504 进制转换
+ * ------
+ * return Integer.toString(num, x);  将 num 转为 x 进制, 且分正负, 格式为 string
+ */
+class Solution {
+    public String convertToBase7(int num) {
+        //return Integer.toString(num, 7);
+
+        return "";
+    }
+}
