6d275587145006d59ab2f36d3c0fc9cad0517a37
yuyang <447395536@qq.com>
2020-03-11 19:05:08 +0800

    680. Valid Palindrome II (Easy)

--- src/cn/yydcyy/_1thinking/_1_doublePointers/_2/SumOfSquareNumbers.java
+++ src/cn/yydcyy/_1thinking/_1_doublePointers/_2/SumOfSquareNumbers.java
@@ -11,7 +11,7 @@ package cn.yydcyy._1thinking._1_doublePointers._2;
 
  本题和 167. Two Sum II - Input array is sorted 类似，只有一个明显区别：一个是和为 target，一个是平方和为 target。本题同样可以使用双指针得到两个数，使其平方和为 target。
 
- 本题的关键是右指针的初始化，实现剪枝，从而降低时间复杂度。设右指针为 x，左指针固定为 0，为了使 02 + x2 的值尽可能接近 target，我们可以将 x 取为 sqrt(target)。
+ 本题的关键是右指针的初始化从，实现剪枝，从而降低时间复杂度。设右指针为 x，左指针固定为 0，为了使 02 + x2 的值尽可能接近 target，我们可以将 x 取为 sqrt(target)。从 c 开始会溢出
 
  因为最多只需要遍历一次 0~sqrt(target)，所以时间复杂度为 O(sqrt(target))。又因为只使用了两个额外的变量，因此空间复杂度为 O(1)。
 */
--- src/cn/yydcyy/_1thinking/_1_doublePointers/_4/Solution.java
+++ src/cn/yydcyy/_1thinking/_1_doublePointers/_4/Solution.java
@@ -1,14 +1,20 @@
 package cn.yydcyy._1thinking._1_doublePointers._4;
 
 /**
-LeetCode 680. 验证回文字符串 Ⅱ
+ 680. Valid Palindrome II (Easy)
+ 题目描述：可以删除一个字符，判断是否能构成回文字符串。
+ 本题的关键是处理删除一个字符。在使用双指针遍历字符串时，如果出现两个指针指向的字符不相等的情况，我们就试着删除一个字符，再判断删除完之后的字符串是否是回文字符串。
+
+ 在判断是否为回文字符串时，我们不需要判断整个字符串，因为左指针左边和右指针右边的字符之前已经判断过具有对称性质，所以只需要判断中间的子字符串即可。
+
+ 在试着删除字符时，我们既可以删除左指针指向的字符，也可以删除右指针指向的字符。
 */
 class Solution {
     public boolean validPalindrome(String s) {
         // 题目说了"给定一个非空字符串 s", 不需要边界处理了.
         for (int i = 0, j = s.length() - 1; i <= j; i ++, j -- ) {
             if (s.charAt(i) != s.charAt(j)) {
-                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1); //这一步是核心, 处理删除
             }
         }
         return true;
